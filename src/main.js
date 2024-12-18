93584c1aefffda604881db2e2229d69d7e2e5c9c
Daniil Tsivinsky <daniil@tsivinsky.com>
2024-12-18 18:12:17 +0300

    Remove printing search params

--- src/main.js
+++ src/main.js
@@ -13,10 +13,6 @@ function main() {
   const link = searchParams.get("link");
   if (!link) return;
 
-  const div = document.createElement("div");
-  div.innerHTML = searchParams.toString();
-  document.body.appendChild(div);
-
   try {
     const u = new URL(decodeURIComponent(link));
     if (u.hostname !== "store.steampowered.com") {
